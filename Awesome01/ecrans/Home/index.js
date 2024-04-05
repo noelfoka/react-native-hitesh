d7c540dd3758340fd6abdd966e8538517fceea79
noelfoka <nomgnenoel@gmail.com>
2024-04-05 16:08:22 +0200

    update1

--- Awesome01/ecrans/Home/index.js
+++ Awesome01/ecrans/Home/index.js
@@ -1,12 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text } from 'react-native';
 
 const HomeScreen = () => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
