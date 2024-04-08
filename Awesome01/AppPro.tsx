edfacda712b570c98323bff1894a9351b6e85c97
noelfoka <nomgnenoel@gmail.com>
2024-04-08 05:54:32 +0100

    finish my app

---
+++ Awesome01/AppPro.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {View, Text, StyleSheet, useColorScheme} from 'react-native';
+function AppPro(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.container}>
+      <Text style={isDarkMode ? styles.whiteText : styles.darkText}>App Pro</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  whiteText: {
+    color: '#fff',
+  },
+  darkText: {
+    color: '#000',
+  },
+});
+
+export default AppPro;
