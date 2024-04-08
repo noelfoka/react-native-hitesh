d7756ec2ff41dabcd4d1b8663fff192e65ece9eb
noelfoka <nomgnenoel@gmail.com>
2024-04-08 15:43:45 +0100

    add description

--- styler02/components/FancyCard.tsx
+++ styler02/components/FancyCard.tsx
@@ -7,6 +7,16 @@ function FancyCard() {
       <Text style={styles.headingText}>Trending places</Text>
       <View style={[styles.card, styles.cardElevated]}>
         <Image source={require('../img/house.jpg')} style={styles.cardImage} />
+        <View style={styles.cardBody}>
+          <Text style={styles.cardTitle}>Douala, Cameroon</Text>
+          <Text style={styles.cardLabel}>Coast Region of Cameroon</Text>
+          <Text style={styles.cardDescription}>
+            Douala is the economic capital of Cameroon. This cosmopolitan city
+            of about 3 million inhabitants concentrates almost 20% of the urban
+            population of the country{' '}
+          </Text>
+          <Text style={styles.cardFooter}>20 minutes away</Text>
+        </View>
       </View>
     </View>
   );
@@ -23,6 +33,11 @@ const styles = StyleSheet.create({
   cardImage: {
     height: 200,
   },
+  cardBody: {},
+  cardTitle: {},
+  cardLabel: {},
+  cardDescription: {},
+  cardFooter: {},
 });
 
 export default FancyCard;
